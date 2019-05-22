e5ecaa452ce49e3c4ba98297dfebb368a3bf3268
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-23 01:01:59 +0300

    Update SorterParagraphsSentenseCount.java

--- src/by/epam/texthandling/sorter/SorterParagraphsSentenseCount.java
+++ src/by/epam/texthandling/sorter/SorterParagraphsSentenseCount.java
@@ -4,21 +4,36 @@ import by.epam.texthandling.composite.Component;
 import by.epam.texthandling.composite.WordComposite;
 import by.epam.texthandling.exception.WrongDataComponentException;
 
-public class SorterParagraphsSentenseCount {
+public class SorterParagraphsSentenseCount implements SorterComposit {
 
-    public Component sortByCountSentence(Component component) throws WrongDataComponentException {
-        if(component == null || !(component instanceof Component)){
+
+    public Component sort(Component component) throws WrongDataComponentException {
+        if (component == null || !(component instanceof Component)) {
             throw new WrongDataComponentException("Component for sort is null");
         }
 
-        Component component1 = new WordComposite();
-        for(Component text : ((WordComposite)component).getComponents()){
-            for(int i = 1; i < ((WordComposite)text).getComponents().size(); i++){
-           //     if(((WordComposite)text).getComponents().get(i) <
+        Component minComponent;
+        Component currentComponent;
+        int min;
+        int current;
+        for (Component textAll : ((WordComposite) component).getComponents()) {
+
+            for (int i = 0; i < ((WordComposite) textAll).getComponents().size() - 1; i++) {
+                minComponent = ((WordComposite) textAll).getComponents().get(i);
+                min = ((WordComposite) minComponent).size();
+                for (int j = i + 1; j < ((WordComposite) textAll).getComponents().size(); j++) {
+                    currentComponent = ((WordComposite) textAll).getComponents().get(j);
+                    current = (((WordComposite) currentComponent).size());
+                    if (current < min) {
+                        ((WordComposite) textAll).getComponents().set(i, currentComponent);
+                        ((WordComposite) textAll).getComponents().set(j, minComponent);
+                        min = current;
+                    }
+                }
             }
         }
-        return component1;
-        }
-        }
+        return component;
+    }
+}
 
 
