67e13e3d7d9f3344fff3734ebca6c7fb6b9b1fed
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-26 13:12:40 +0300

    Update LetterPunktParcer.java

--- src/by/epam/texthandling/handler/LetterPunktParcer.java
+++ src/by/epam/texthandling/handler/LetterPunktParcer.java
@@ -1,13 +1,18 @@
 package by.epam.texthandling.handler;
 
 import by.epam.texthandling.composite.Component;
-import by.epam.texthandling.composite.Simbol;
-import by.epam.texthandling.composite.WordComposite;
+import by.epam.texthandling.composite.Symbol;
+import by.epam.texthandling.composite.TextComposite;
+import by.epam.texthandling.composite.TypeComponent;
 import by.epam.texthandling.exception.TextParcerException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LetterPunktParcer implements TextParcer {
 
-    private Component letter;
+    private Component symbol;
+    private static final Logger LOGGER = LogManager.getLogger();
+
 
     @Override
     public Component handleRequest(String text) throws TextParcerException {
@@ -15,13 +20,14 @@ public class LetterPunktParcer implements TextParcer {
             throw new TextParcerException("Text for parce is null");
         }
 
-        letter = new WordComposite();
+        symbol = new TextComposite(TypeComponent.SYMBOL);
         char[] simbols = text.toCharArray();
 
-        for(char x : simbols){
-            letter.add(new Simbol(x));
+        for(char element : simbols){
+            LOGGER.debug("{ " + element + " } was added to" + ((TextComposite)symbol).getTypeComponent());
+            symbol.add(new Symbol(element));
         }
 
-        return letter;
+        return symbol;
     }
 }
