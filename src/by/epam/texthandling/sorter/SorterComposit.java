8469b8bdca744e2cd88454e9bb63d319bff1ab97
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-26 13:17:32 +0300

    Update SorterComposit.java

--- src/by/epam/texthandling/sorter/SorterComposit.java
+++ src/by/epam/texthandling/sorter/SorterComposit.java
@@ -1,9 +1,47 @@
 package by.epam.texthandling.sorter;
 
 import by.epam.texthandling.composite.Component;
+import by.epam.texthandling.composite.TextComposite;
+import by.epam.texthandling.composite.TypeComponent;
 import by.epam.texthandling.exception.WrongDataComponentException;
 
-public interface SorterComposit {
+public abstract class SorterComposit {
 
-    Component sort(Component component) throws WrongDataComponentException;
+    public abstract void sort(Component component) throws WrongDataComponentException;
+
+    protected void sortHelper(Component component, TypeComponent typeComponent){
+        for(Component part : ((TextComposite)component).getComponents()){
+            if(((TextComposite)part).getTypeComponent() != typeComponent){
+                sortHelper(part, typeComponent);
+            } else{
+                Component minComponent;
+                Component currentComponent;
+                Component temp;
+                int minSize;
+                int current;
+
+                for(Component currentParagr : ((TextComposite)part).getComponents()){
+                    for (int i = 0; i < ((TextComposite)part).size(); i++){
+                        minComponent = ((TextComposite)part).getComponents().get(i);;
+                        minSize = ((TextComposite)minComponent).size();
+
+                        for(int j = i + 1; j < ((TextComposite)part).size(); j++){
+                            currentComponent = ((TextComposite)part).getComponents().get(j);
+                            current = ((TextComposite)currentComponent).size();
+                            temp = new TextComposite(typeComponent);
+                            if (current < minSize) {
+                                ((TextComposite)temp).add(((TextComposite) part).getComponents().get(i));
+                                ((TextComposite) part).getComponents().set(i, currentComponent);
+                                minSize = current;
+                                ((TextComposite) part).getComponents().set(j, ((TextComposite) temp).getComponents().get(0));
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+    }
 }
