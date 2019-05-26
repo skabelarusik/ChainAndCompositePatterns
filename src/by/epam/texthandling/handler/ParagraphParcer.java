360db5c5b1698b86c46f7dcf368dee4e3b18ea2d
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-26 13:13:21 +0300

    Update ParagraphParcer.java

--- src/by/epam/texthandling/handler/ParagraphParcer.java
+++ src/by/epam/texthandling/handler/ParagraphParcer.java
@@ -1,14 +1,19 @@
 package by.epam.texthandling.handler;
 
 import by.epam.texthandling.composite.Component;
-import by.epam.texthandling.composite.WordComposite;
+import by.epam.texthandling.composite.TextComposite;
+import by.epam.texthandling.composite.TypeComponent;
 import by.epam.texthandling.exception.TextParcerException;
-import by.epam.texthandling.resources.RegularExpression;
+import by.epam.texthandling.resource.RegularExpression;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ParagraphParcer implements TextParcer {
 
     private TextParcer nextParcer;
     private Component paragraphsComposite;
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public Component handleRequest(String text) throws TextParcerException {
@@ -17,13 +22,14 @@ public class ParagraphParcer implements TextParcer {
             throw new TextParcerException("Text for parce is null");
         }
 
-        paragraphsComposite = new WordComposite();
+        paragraphsComposite = new TextComposite(TypeComponent.PARAGRAPH);
 
             nextParcer = new SentenceParcer();
             String[] paragraphs = text.split(RegularExpression.PARAGRAPH_SPLIT_REG_EX);
 
-            for (String x : paragraphs) {
-                paragraphsComposite.add(nextParcer.handleRequest(x.trim()));
+            for (String paragr : paragraphs) {
+                LOGGER.debug("{ " + paragr + " } was added to" + ((TextComposite)paragraphsComposite).getTypeComponent());
+                paragraphsComposite.add(nextParcer.handleRequest(paragr.trim()));
             }
 
         return paragraphsComposite;
