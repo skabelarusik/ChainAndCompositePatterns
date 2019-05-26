0fa932b1ba2a750aa8d430cc43652c9b0e8f6853
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-26 13:17:56 +0300

    Update SorterSentenceWordCount.java

--- src/by/epam/texthandling/sorter/SorterSentenceWordCount.java
+++ src/by/epam/texthandling/sorter/SorterSentenceWordCount.java
@@ -1,38 +1,23 @@
 package by.epam.texthandling.sorter;
 
 import by.epam.texthandling.composite.Component;
-import by.epam.texthandling.composite.WordComposite;
+import by.epam.texthandling.composite.TextComposite;
+import by.epam.texthandling.composite.TypeComponent;
 import by.epam.texthandling.exception.WrongDataComponentException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class SorterSentenceWordCount implements SorterComposit {
+public class SorterSentenceWordCount extends SorterComposit {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Override
-    public Component sort(Component component) throws WrongDataComponentException {
+    public void sort(Component component) throws WrongDataComponentException {
        if(component == null){
            throw new WrongDataComponentException("Component for sort sentences by words is null");
        }
+        sortHelper(component, TypeComponent.SENTENCE);
+        LOGGER.debug(((TextComposite)component).getTypeComponent() + " was sorted");
 
-        Component minComponent;
-        Component currentComponent;
-        int minSize;
-        int current;
-        for (Component textAll : ((WordComposite) component).getComponents()) {
-            for(Component paragr : ((WordComposite)textAll).getComponents()){
-                for (int i = 0; i < ((WordComposite) paragr).getComponents().size() - 1; i++) {
-                minComponent = ((WordComposite) paragr).getComponents().get(i);
-                minSize = ((WordComposite) minComponent).size();
-                    for (int j = i + 1; j < ((WordComposite) paragr).getComponents().size(); j++) {
-                        currentComponent = ((WordComposite) paragr).getComponents().get(j);
-                        current = (((WordComposite) currentComponent).size());
-                        if (current < minSize) {
-                            ((WordComposite) paragr).getComponents().set(i, currentComponent);
-                            minSize = current;
-                            ((WordComposite) paragr).getComponents().set(j, minComponent);
-                        }
-                    }
-                }
-            }
-        }
-
-       return component;
     }
 }
