f03228360485875e282272d5870753f6a11b4fb2
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-26 13:15:26 +0300

    Update RegularExpression.java

--- src/by/epam/texthandling/resources/RegularExpression.java
+++ src/by/epam/texthandling/resources/RegularExpression.java
@@ -1,9 +1,9 @@
-package by.epam.texthandling.resources;
+package by.epam.texthandling.resource;
 
 public class RegularExpression {
 
     public static final String PARAGRAPH_SPLIT_REG_EX = "(?m)(?=^\\s{4})";
-    public static final String SENTENCE_SPLIT_REG_EX = "(?<=\\.)";
+    public static final String SENTENCE_SPLIT_REG_EX = "(?<=(\\.|\\!|\\?|\u2620))";
     public static final String WORD_SPLIT_REG_EX = "\\s+";
-    public static final String PUNKT_SPLIT_REG_EX = " ";
+    public static final String PUNСT_SPLIT_REG_EX = " ";
 }
