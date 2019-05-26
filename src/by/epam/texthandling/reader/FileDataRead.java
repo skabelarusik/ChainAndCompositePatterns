8c59bcf8a490758a83f677432c7d45b16d65d2ca
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-05-26 13:14:56 +0300

    Update FileDataRead.java

--- src/by/epam/texthandling/reader/FileDataRead.java
+++ src/by/epam/texthandling/reader/FileDataRead.java
@@ -1,11 +1,16 @@
 package by.epam.texthandling.reader;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileDataRead {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public static String fileRead (String pathFile) throws FileNotFoundException {
         if(pathFile == null ){
@@ -14,6 +19,7 @@ public class FileDataRead {
 
         String text;
         try {
+      //      LOGGER.debug("File was readed : " + pathFile);
             text = new String(Files.readAllBytes(Paths.get(pathFile)));
         } catch (IOException e) {
             throw new FileNotFoundException("Wrong path");
